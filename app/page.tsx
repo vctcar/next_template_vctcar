da8699e395b8126ed3118b310312c3ff8d4306e5
vcarr <carrasco.ve1998@gmail.com>
2025-02-15 06:52:05 -0800

    running out of ideas

--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,26 @@
-"use client";
+"use client"
 
-import { useState, useEffect } from "react";
-import { useAuthenticator } from "@aws-amplify/ui-react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
+import React from "react";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+import "./../app/app.css";
+import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import outputs from "@/amplify_outputs.json";
 
 Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
-
-export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  const { user, signOut} = useAuthenticator();
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
-
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => ( 
-          <li
-          onClick={() => deleteTodo(todo.id)} 
-           key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button>
-    </main>
+    <html lang="en">
+      <body>      
+        <Authenticator>
+          {children}
+        </Authenticator>
+      </body>
+    </html>
   );
 }
